99d5546799eb288a7eb51dee02d751bbbfa6f205
GusanHaribo <hth99131@gmail.com>
2026-04-21 22:37:56 +0900

    refactor: 점수연산에 불필요한 도메인 삭제

--- apps/stock-api/src/main/java/com/momentum/domain/entity/rank/RankCapacity.java
+++
@@ -1,6 +0,0 @@
-package com.momentum.domain.entity.rank;
-
-public final class RankCapacity {
-
-  private static final int RANK_LIMIT = 100;
-}
--- apps/stock-api/src/main/java/com/momentum/domain/entity/rank/StockIndividualExpectedReturn.java
+++
@@ -1,9 +0,0 @@
-package com.momentum.domain.entity.rank;
-
-import com.momentum.domain.BaseEntity;
-import jakarta.persistence.Entity;
-
-@Entity
-public class StockIndividualExpectedReturn  extends BaseEntity {
-  private long value;
-}
