3615a2256c78cf427bc737cef97fb24c5226ac43
GusanHaribo <hth99131@gmail.com>
2026-05-02 23:28:11 +0900

    refactor: StockDailyRegimeService 분리

--- core/stock/src/main/java/com/momentum/domain/stock/StockDailyRegimeService.java
+++ core/stock/src/main/java/com/momentum/domain/stock/StockDailyRegimeService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 public class StockDailyRegimeService {
 
   private static final double BREAKOUT_THRESHOLD = 3.0;
-  private static final double APPROACH_THRESHOLD = 3.0;
+  private static final double LINE_APPROACH_THRESHOLD = 3.0;
 
   private final StockRepository stockRepository;
   private final StockBaseRepository stockBaseRepository;
@@ -22,71 +22,19 @@ public class StockDailyRegimeService {
 
   public void finalizeDailyState(StockDailyCandle stockDailyCandle) {
     Stock stock = stockDailyCandle.getStock();
-    long closePrice = stockDailyCandle.getClosePrice();
 
     Optional<StockBase> currentBaseOpt = stockBaseRepository.findCurrentBaseWithLines(stock);
-    if (currentBaseOpt.isEmpty()) {
-      stock.update(StockRegime.UNDETERMINED);
-      stockRepository.save(stock);
-      return;
-    }
-
-    StockBase currentBase = currentBaseOpt.get();
-
     Optional<StockPricePoint> recentPricePointOpt = stockPricePointRepository.findLatestByStock(stock);
-    if (recentPricePointOpt.isEmpty()) {
+    if (currentBaseOpt.isEmpty() || recentPricePointOpt.isEmpty()) {
       stock.update(StockRegime.UNDETERMINED);
       stockRepository.save(stock);
       return;
     }
 
-    long recentPivotPrice = recentPricePointOpt.get().getPrice();
-    long resistancePrice = currentBase.getHighestResistanceLine().getPrice();
-    long supportPrice = currentBase.getLowestSupportLine().getPrice();
-
-    StockRegime regime = determineRegime(stock, closePrice, recentPivotPrice, resistancePrice, supportPrice, currentBase);
-    stock.update(regime);
+    StockRegime newRegime = StockRegime.determineRegime(stock,
+        stockDailyCandle.getClosePrice(),
+        recentPricePointOpt.get(), currentBaseOpt.get(), BREAKOUT_THRESHOLD, LINE_APPROACH_THRESHOLD);
+    stock.update(newRegime);
     stockRepository.save(stock);
-  }
-
-  private StockRegime determineRegime(Stock stock, long closePrice, long recentPivotPrice,
-      long resistancePrice, long supportPrice, StockBase currentBase) {
-
-    // # 하방이탈: (종가 < 지지선) AND (종가 < PP)
-    if (closePrice < supportPrice && closePrice < recentPivotPrice) {
-      return StockRegime.DOWNSIDE_BREAK;
-    }
-
-    // # 돌파실패: (종가 < 저항선) AND (종가 < PP)
-    if (closePrice < resistancePrice && closePrice < recentPivotPrice) {
-      return StockRegime.BREAKOUT_FAILED;
-    }
-
-    // # 돌파시작: (상승 템플릿) AND (종가 > 저항선 +3%) AND (종가 > PP)
-    if (stock.getStockTrend().equals(StockTrend.UPTREND)
-        && calculateGap(resistancePrice, closePrice) > BREAKOUT_THRESHOLD
-        && closePrice > recentPivotPrice) {
-      return StockRegime.BREAKOUT_START;
-    }
-
-    // # 돌파준비: (상승 템플릿) AND (VCP OR (저항선 -3% 이상 AND 종가 > PP))
-    if (stock.getStockTrend().equals(StockTrend.UPTREND)) {
-      boolean isVcp = currentBase.getVcp().isVcp();
-      boolean isApproaching = calculateGap(resistancePrice, closePrice) >= -APPROACH_THRESHOLD
-          && closePrice > recentPivotPrice;
-      if (isVcp || isApproaching) {
-        return StockRegime.BREAKOUT_READY;
-      }
-    }
-
-    // # 방향미정: 위 4개 케이스 미해당
-    return StockRegime.UNDETERMINED;
-  }
-
-  private double calculateGap(long linePrice, long currentPrice) {
-    if (linePrice == 0) {
-      return 0.0;
-    }
-    return ((double) (currentPrice - linePrice) / linePrice) * 100;
   }
 }
