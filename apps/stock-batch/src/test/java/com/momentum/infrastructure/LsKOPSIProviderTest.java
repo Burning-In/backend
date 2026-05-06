82254bfc8fb587f20fd2cb21a3f1863f10a45926
GusanHaribo <hth99131@gmail.com>
2026-05-07 01:29:52 +0900

    refactor: disable 및 필요없는 파일제거

--- apps/stock-batch/src/test/java/com/momentum/infrastructure/LsKOPSIProviderTest.java
+++ apps/stock-batch/src/test/java/com/momentum/infrastructure/LsKOPSIProviderTest.java
@@ -2,10 +2,12 @@ package com.momentum.infrastructure;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class LsKOPSIProviderTest {
 
